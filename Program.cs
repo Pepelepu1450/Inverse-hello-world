941fbc3fdcf3f6e7be0020186d3b1045016a5540
pepe <jose.vacaslocas@gmail.com>
2020-03-05 19:34:23 -0700

    hello world inverso

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Inverse_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string helloWorld = "Hello World!";
+            int charCount = helloWorld.Length;
+            char [] caracteresHelloWorld = new char [charCount];
+
+            //caracteresHelloWorld[0]
+
+            //Console.WriteLine(helloWorld);
+
+            //Console.WriteLine(helloWorld.Substring(0, 5));
+
+            //Console.WriteLine(helloWorld[0]);
+
+            for (int i = 0; i < charCount; i++){
+                Console.WriteLine(helloWorld[i]);
+
+                caracteresHelloWorld[i] = helloWorld[i];
+            }
+
+            Console.WriteLine(caracteresHelloWorld);
+        }
+    }
+}
